4b9c05a0cefee4243a1e0056e3a80f09c4dc4e27
Guillaume Sorbier <guillaume@guibru.sh>
2014-09-22 19:55:18 +0200

    Finalisation de la première version
    Les liens dans la navigation sont fonctionnels
    Ajout d’un script de validation de formulaire (non fonctionnel)

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,15 @@
 //= require_tree .
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('nav a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// http://reactiveraven.github.io/jqBootstrapValidation/
+$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); } );
