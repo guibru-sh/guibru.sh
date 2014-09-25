7292552d9c01fb71918fb0ea771544c40b2676c9
Guillaume Sorbier <guillaume@guibru.sh>
2014-09-25 17:40:59 +0200

    Contact form finished
    Fixes #2

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -13,3 +13,44 @@ $(function() {
 
 // http://reactiveraven.github.io/jqBootstrapValidation/
 $(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); } );
+
+// $(function () { $("#contactForm").validate(); } );
+
+$(document).ready(function() {
+    $('#contactForm')
+      .on('success.form.bv', function(e) {
+        // Prevent form submission
+        e.preventDefault();
+
+        // Get the form instance
+        var $form = $(e.target);
+
+        // Get the BootstrapValidator instance
+        var bv = $form.data('bootstrapValidator');
+
+        // Use Ajax to submit form data
+        // https://zapier.com/hooks/catch/o6ufcm/
+        $.post("https://zapier.com/hooks/catch/o6ufcm/", $form.serialize(), function(result) {
+            // ... Process the result ...
+        }, 'json').done(function() {
+          $('#contactModal').modal();
+          $('#contactForm').data('bootstrapValidator').resetForm();
+          $('#contactForm').trigger("reset");
+        });
+      })
+      .bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+        fields: {
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email address is required and cannot be empty'
+                    },
+                    emailAddress: {
+                        message: 'The email address is not a valid'
+                    }
+                }
+            }
+        }
+    });
+});
