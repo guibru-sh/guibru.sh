18d0a1e41bbffeb4833a655ad9f8d16e9622cffe
Guillaume Sorbier <guillaume@guibru.sh>
2014-11-06 16:52:25 +0100

    Add script for Agile CRM tracker

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -17,40 +17,46 @@ $(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValid
 // $(function () { $("#contactForm").validate(); } );
 
 $(document).ready(function() {
-    $('#contactForm')
-      .on('success.form.bv', function(e) {
-        // Prevent form submission
-        e.preventDefault();
+  $('#contactForm')
+  .on('success.form.bv', function(e) {
+    // Prevent form submission
+    e.preventDefault();
 
-        // Get the form instance
-        var $form = $(e.target);
+    // Get the form instance
+    var $form = $(e.target);
 
-        // Get the BootstrapValidator instance
-        var bv = $form.data('bootstrapValidator');
+    // Get the BootstrapValidator instance
+    var bv = $form.data('bootstrapValidator');
 
-        // Use Ajax to submit form data
-        // https://zapier.com/hooks/catch/o6ufcm/
-        $.post("https://zapier.com/hooks/catch/o6ufcm/", $form.serialize(), function(result) {
-            // ... Process the result ...
-        }, 'json').done(function() {
-          $('#contactModal').modal();
-          $('#contactForm').data('bootstrapValidator').resetForm();
-          $('#contactForm').trigger("reset");
-        });
-      })
-      .bootstrapValidator({
-        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-        fields: {
-            email: {
-                validators: {
-                    notEmpty: {
-                        message: 'The email address is required and cannot be empty'
-                    },
-                    emailAddress: {
-                        message: 'The email address is not a valid'
-                    }
-                }
-            }
-        }
+    // Use Ajax to submit form data
+    // https://zapier.com/hooks/catch/o6ufcm/
+    $.post("https://zapier.com/hooks/catch/o6ufcm/", $form.serialize(), function(result) {
+      // ... Process the result ...
+    }, 'json').done(function() {
+      $('#contactModal').modal();
+      $('#contactForm').data('bootstrapValidator').resetForm();
+      $('#contactForm').trigger("reset");
     });
+    // Agile CRM - Setting Email of the page visitor
+    _agile.set_email($('#email').val());
+    // Agile CRM - Create a new contact
+    $.post("https://zapier.com/hooks/catch/o952dk/", $form.serialize(), function(result) {
+      // ... Process the result ...
+    }, 'json');
+  })
+  .bootstrapValidator({
+    // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+    fields: {
+      email: {
+        validators: {
+          notEmpty: {
+            message: 'The email address is required and cannot be empty'
+          },
+          emailAddress: {
+            message: 'The email address is not a valid'
+          }
+        }
+      }
+    }
+  });
 });
